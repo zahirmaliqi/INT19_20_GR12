70a29cfcfdb1043dbc018fecfe9a0322f967aba7
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-04-01 13:41:10 +0200

    Create menuu.php

---
+++ menuu.php
@@ -0,0 +1,45 @@
+<header style="width: 100%">
+
+<div class="ikonat" style="float: left;">
+		
+		<a href="https://www.linkedin.com/" class="fa fa-linkedin"></a>
+		<a href="http://twitter.com" class="fa fa-twitter"></a>
+		<a href="http://pinterest.com" class="fa fa-pinterest"></a>
+		<a href="http://google.com" class="fa fa-google"></a>
+		<a href="https://rss.com/" class="fa fa-rss"></a>
+
+  </div>
+  <div style="float: right">
+    <form>
+	     
+           <input type="text" name="text" class="search" placeholder="Search Our Website..."  >
+          
+           <input type="submit" name="submit" class="submit" value="SEARCH">
+       </form>
+   </div>
+   <br>
+   <br>
+     </header>
+
+	   <section class="faqja" >
+		<nav id="mainMenu">
+	   <div class="h6">
+		<p id="fresh"><b><big>M</big>ANCHESTER<big>U</big>NITED</b></p>
+		<p id="minfont">The best team!</p>
+      
+       </div>
+       <div >
+       <img src="mmm.png" alt="Trulli" id="man" style="width:70px; height: 70px; border-radius:50%; " >
+       </div>
+		
+		
+
+                      <ul>
+                            <li><a href="Faqja1.php" >HOMEPAGE</a></li>
+                            <li> <a href="Historyandcuriosities.php">HISTORY AND CURIOSITIES</a> </li>
+			    <li><a href="pjesa2.php">PLAYERS</a>  </li>
+                            <li>  <a href="pic.php">GALLERY</a></li>
+                            <li> <a href="Tickets.php">FIXTURES</a> </li>
+                            <li> <a href="Game.php">GAME</a> </li>
+                       </ul> 
+          </nav>
