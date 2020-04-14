7a9dad5694c11de32571a4698604b60c6e3a3e4e
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-04-14 16:37:18 +0200

    Validimi i formes

--- buyticket.php
+++ buyticket.php
@@ -3,12 +3,19 @@
   <meta http-equiv="content-type" content="text/html"; charset="utf-8"/>
     <title>Login Form</title>
     <link href="buyticket.css" rel="stylesheet" type="text/css"/>
+<style>
+.error{
+	
+	color:red;
+}
+</style>
 
 
     </head>
     <body>
-    <div class="tiketa">
-         <form name="myForm" onsubmit="return validateForm()" method="post">
+      <?php include("form.php"); ?>  //validimi i te dhenave te perdoruesit
+    <div class="tiketa" style="height:650px;width:250px;">
+         <form name="myForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <h3 style="color:#fff;">
           <?php
             $txt = "Buy Tickets or T-shirt";
@@ -17,50 +24,41 @@
             ?>
            </h3>
         <h7 style="color:white">Fill the details below accurately and we will contact you soon.</h7>
-            <input type="text" name="username" placeholder=" Name" required autofocus>
+           <input type="text" name="username" placeholder=" Name">
+			<span class="error"><?php echo $nameErr;?></span>
             <br><br>
-            <input type="text" name="username" placeholder="Last Name">
+            <input type="text" name="lastname" placeholder="Last Name">
             <br><br>
-            <input type="tel" name="Phone number"
-                   placeholder="Phone number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required><br><br>
+            <input type="tel" name="number"
+                   placeholder="Phone number">			
+			<span class="error"><?php echo $numberErr;?></span>
+				   <br><br>
            <input type="password" name="code"
-                   placeholder="Your code"><br><br>
-            <input type="email" name="email" placeholder="Email address" autocomplete="off" required><br><br>
-              <input type="checkbox" name="vehicle1" value="Bike"> I accept terms and condition.<br><br>
+                   placeholder="Your code">
+			<span class="error"><?php echo $codeErr;?></span>	   
+				   <br><br>
+            <input type="email" name="email" placeholder="Email address" autocomplete="off" />
+			<span class="error"><?php echo $emailErr;?></span>
+			<br><br>
+              <input type="checkbox" name="vehicle1" value="Bike"> I accept terms and condition.</input>
+			  <span class="error"><?php echo $termsErr;?></span>
+			  <br><br>
               <select class="sty">
   <option value="Select">Select:</option>
   <option value="T-Shirt">T-Shirt</option>
   <option value="Ticket">Ticket</option>
  
                </select>
-               <br>
-         
+               <br>  
         </br>
-         
             <input type="submit" formmethod="post" value="BUY NOW" onclick="myFunction()"/><br><br>
-            
+            <h7>Please fill out all the information asked in this form!</h7>
             <script>
                 function myFunction() {
-                alert("Are all requirements fulfilled?");
+                alert("Are all the requirements fulfilled?");
                                        }
-            </script>
-            
+            </script>        
         </form>
         </div>
-     
-     <script>
-function validateForm() {
-  var x = document.forms["myForm"]["code"].value;
-  if (x == "") {
-    alert("You must enter your code to continue.");
-    return false;
-  }
-}
-</script>
-    <script>
-var str = prompt("Type your favourite team:"); 
-var n = str.search(/a/i);
-alert("The position of a in "+str+" is: " + n);
-</script>
     </body>
 </html>
