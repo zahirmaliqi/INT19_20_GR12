0bbd3846800057b8592845e19e30acfbc85fbf2f
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-04-16 23:02:56 +0200

    Update form.php

--- form.php
+++ form.php
@@ -8,34 +8,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nameErr = "*Name is required";
   } else {
     $name = test_input($_POST["username"]);
+if(!preg_match('/^[A-Za-z]+/',$name))
+{
+	$nameErr ="*Name is in a wrong format";
+}
+
   }
   
   if (empty($_POST["number"])) {
     $numberErr = "*Phone number is required";
   }
-else if(preg_match('/[0-9]{3}-[0-9]{3}-[0-9]{4}$/',$number))
-{
-	$numberErr ="*Number is in a wrong format";
-}
 
   else {
     $number = test_input($_POST["number"]);
+if(!preg_match('/[0-9]{3}-[0-9]{3}-[0-9]{4}$/',$number))
+{
+	$numberErr ="*Number is in a wrong format";
+}
   }
     
   if (empty($_POST["code"])) {
     $codeErr = "*A code is required";
-  } else {
+  } 
+else if(strlen($_POST["code"])<6||strlen($_POST["code"])>20){
+   $codeErr="Your code should be between 6 and 20 characters";
+  }	
+else {
     $code = test_input($_POST["code"]);
   }
 
   if (empty($_POST["email"])) {
-  $emailErr = "*Email is required";}
-	else if(preg_match('/^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/',$email))
+  $emailErr = "*Email is required";
+  }
+	
+  else {
+    $email = test_input($_POST["email"]);
+if(!preg_match('/^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/',$email))
 {
 	$emailErr ="*Email is in a wrong format";
 }
-  else {
-    $email = test_input($_POST["email"]);
   }
 
   if (empty($_POST["vehicle1"])) {
