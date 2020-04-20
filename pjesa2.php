f3695b4d0e6735f6c669358fd9f47b92c29a8171
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-04-21 01:52:32 +0200

    Update pjesa2.php

--- pjesa2.php
+++ pjesa2.php
@@ -99,9 +99,9 @@
 		
 
                        <ul>
-                            <li><a href="Faqja1.html" >HOMEPAGE</a></li>
+                            <li><a href="Faqja1.php" >HOMEPAGE</a></li>
                             <li> <a href="Historyandcuriosities.html">HISTORY AND CURIOSITIES</a> </li>
-                            <li><a href="pjesa2.html">PLAYERS</a>  </li>
+                            <li><a href="pjesa2.php">PLAYERS</a>  </li>
                             <li>  <a href="pic.html">GALLERY</a></li>
                             <li> <a href="Tickets.html">FIXTURES</a> </li>
                             <li> <a href="Game.html">GAME</a> </li>
