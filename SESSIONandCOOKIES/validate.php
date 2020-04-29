2424c51fb7f0ca9d22ab75edad9158ce87ea9c7b
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-04-30 00:14:30 +0200

    Create validate.php

---
+++ SESSIONandCOOKIES/validate.php
@@ -0,0 +1,25 @@
+<?php 
+
+$myemail = "manutd@gmail.com";
+$mypass = "12345";
+if(isset($_POST['login'])) {
+    $email=$_POST['email'];
+    $pass = $_POST['password'];
+    
+    if($email==$myemail and $pass==$mypass) {
+        if( isset($_POST['remember'])) {
+            setcookie('email',$email, time()+60*60*7);
+            setcookie('pass',$pass, time()+60*60*7);
+            
+        }
+        session_start();
+        $_SESSION['email']=$email;
+        header("location:Faqja1.php");
+    } else {
+        echo "Email or Password is Invailid.<br> click here to <a href='login1.php'>try again</a>";
+    }
+    } else {
+    header ("location:login1.php");
+}
+
+?>
