5db15b0a5bf05db2434e817ea0d604e2e215569c
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-04-30 23:17:44 +0200

    Create poll_vote.php

---
+++ poll_vote.php
@@ -0,0 +1,44 @@
+<?php
+$vote = $_REQUEST['vote'];
+
+$filename = "poll_result.txt";
+$content = file($filename);
+
+$array = explode("||", $content[0]);
+$yes = $array[0];
+$no = $array[1];
+
+if ($vote == 0) {
+  $yes = $yes + 1;
+}
+if ($vote == 1) {
+  $no = $no + 1;
+}
+
+$insertvote = $yes."||".$no;
+$fp = fopen($filename,"w");
+fputs($fp,$insertvote);
+fclose($fp);
+?>
+
+<h3>The results until now are:</h3>
+<table>
+<tr>
+<td>Yes:</td>
+<td>
+<img src="poll.gif"
+width='<?php echo(100*round($yes/($no+$yes),2)); ?>'
+height='20'>
+<?php echo(100*round($yes/($no+$yes),2)); ?>%
+</td>
+</tr>
+<tr>
+<td>No:</td>
+<td>
+<img src="poll.gif"
+width='<?php echo(100*round($no/($no+$yes),2)); ?>'
+height='20'>
+<?php echo(100*round($no/($no+$yes),2)); ?>%
+</td>
+</tr>
+</table>
