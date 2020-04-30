af3dc3c527d750119a7b7bcb894f9dc9ecd500e1
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-04-30 23:38:13 +0200

    Shtimi i disa lajmeve

--- SQL/databazalajmet.php
+++ SQL/databazalajmet.php
@@ -11,7 +11,7 @@ if($conn->connect_error){
 echo "Konektimi i suksesshem";
 
 $sql="CREATE DATABASE lajmet";
-//$sql="DROP DATABASE ushtrime";
+
 $conn->query($sql);
 
 if($conn->query($sql)==TRUE){
@@ -28,9 +28,15 @@ $conn->close();
 
  $conn=mysqli_connect(servername,username,password,dbname);
 
-	 $query="INSERT INTO lajmet(titullilink,pershkrimi)
-	 VALUES('UNITED MAKE CHRISTMAS DREAMS COME TRUE','The Manchester United first-team squad paid a special festive visit...')";
-	
+	 //$query="INSERT INTO lajmet(titullilink,pershkrimi)
+	// VALUES('UNITED MAKE CHRISTMAS DREAMS COME TRUE','The Manchester United first-team squad paid a special festive visit...')";
+	// $query.="INSERT INTO lajmet(titullilink,pershkrimi)
+	 //VALUES('UNITED V AZ ALKMAAR AT OLD TRAFFORD','Manchester United will return to action on Thursday when AZ Alkmaar visit Old Trafford in the UEFA EU League, with the top spot in Group L up for grabs between two sides.');"; */
+  $query="INSERT INTO lajmet(titullilink,pershkrimi)
+	 VALUES('RASHFORD EXPLAINS SUPERB GOALSCORING FORM','Marcus Rashford has revealed that consistency is the reason behind his superb goalscoring form.
+The Manchester United no.10 has put...');";
+  $query.="INSERT INTO lajmet(titullilink,pershkrimi) VALUES('THE MAIN TALKING POINTS FROM THE DERBY','Manchester United enjoyed a superb evening on Saturday, when we emerged from the Etihad with a vital 2-1 win in the Manchester derby.');";
+
 	 $result=mysqli_multi_query($conn,$query);
 	 if(!$result){
 		 die("NUK shtohen tdhenat ".mysqli_error($conn));
