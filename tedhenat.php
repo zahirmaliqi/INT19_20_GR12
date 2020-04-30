0b8ae1048544b0d002d6fd901dad03739ed3d139
vanesarama <56617704+vanesarama@users.noreply.github.com>
2020-04-30 15:03:17 +0200

    Update tedhenat.php

--- tedhenat.php
+++ tedhenat.php
@@ -84,3 +84,31 @@ echo "<br>";
 
    </body>
         </html>
+    <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+//require_once("dbconfig.php");
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+$sql = "CREATE DATABASE myDB";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+    ?>
