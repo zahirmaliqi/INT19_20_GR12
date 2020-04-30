664696574b3ec06e783a93e3944d5f77c9b449b6
vanesarama <56617704+vanesarama@users.noreply.github.com>
2020-04-30 16:54:24 +0200

    Update tedhenat.php
    CreateTabel

--- tedhenat.php
+++ tedhenat.php
@@ -111,4 +111,17 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+    // sql to create table
+$sql = "CREATE TABLE tiketa (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+code VARCHAR(256) NOT NULL,
+email VARCHAR(50) NOT NULL,
+phonenumber VARCHAR(256)NOT NULL,
+buy VARCHAR(20)NOT NULL,
+
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
     ?>
