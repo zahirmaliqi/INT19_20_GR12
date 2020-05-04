a271c77147ff7a37c1c20a606e180b98d96ba6e7
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-05-04 23:43:38 +0200

    Create jscript.js

---
+++ jscript.js
@@ -0,0 +1,19 @@
+function showCD(str) {
+  if (str=="") {
+    document.getElementById("txtHint").innerHTML="";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    
+    xmlhttp=new XMLHttpRequest();
+  } else {
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("txtHint").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","getcd.php?q="+str,true);
+  xmlhttp.send();
+}
