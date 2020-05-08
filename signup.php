9b49524a9204bc309fb9ec3fc212a08f0179a7f4
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-05-08 16:32:47 +0200

    Sign up permes Superglobals

--- signup.php
+++ signup.php
@@ -3,24 +3,33 @@
 	<head>
 		<title>Signup</title>
 		
-		<link rel="stylesheet" type="text/css" href="signup.css">
+		<link rel="stylesheet"  href="signup.css">
+		<form action="inc.php" method="POST" id="registration" >
+                <h2 style="color:white;">Sign up</h2>
+                <?php
+                if(isset($_GET['first'])){
+                    $first=$_GET['first'];
+                    echo '<input type="text" name="first" placeholder="First name" id="button value="'.$first.'"><br><br>';
+                }
+                else {
+                    echo '<input type="text" name="first" placeholder="First name" id="button"><br><br>';
+                }
+                if(isset($_GET['last'])){
+                    $last=$_GET['last'];
+                    echo '<input type="text" name="last" placeholder="Last name" id="button" value="'.$last.'"><br><br>';
+                }
+                else {
+                    echo '<input type="text" name="last" placeholder="Last name" id="button"><br><br>';
+                }
+              
+                ?>
+					<input type="email" name="email" placeholder="Email" id="button"><br><br>
+					<input type="password" name="pwd" placeholder="Password" id="button"><br><br>
+				    <input type="submit" name="submit" id="butto"><br><br>
+				    <p id="sub" style="color: white;"></p>
+				    <p style="color: white;">Already have an account? &nbsp;<a href="login1.php" id="log">Log In</a></p>
+                    <?php
 
-		<script>
-			function myFunction() {
-				  var x, text;
-
-				  // Get the value of the input field with id="numb"
-				  x = document.getElementById("phone").value;
-
-				  // If x is Not a Number or less than one or greater than 10
-				  if (isNaN(x)) {
-				    text = "Input not valid";
-				  } else {
-				    text = "Input OK";
-				  }
-				  document.getElementById("sub").innerHTML= text;
-				}
-		</script>
 
 
 	</head>
@@ -28,21 +37,7 @@
 		
 			<div class="simple-form">
 
-				<form id="registration">
-
-					<input type="text" name="fname" placeholder="First name" id="button" required><br><br>
-					<input type="text" name="lname" placeholder="Last name" id="button" required><br><br>
-					<input type="email" name="email" placeholder="Email" id="button" required><br><br>
-					<input type="password" name="password" placeholder="Password" id="button" required><br><br>
-					<select id="ph"><datalist><option>+383</option><option>+386</option><option>+377</option></datalist>
-				</select>
-				<input  name="num" placeholder="Enter your phone number" id="phone" pattern="\d{8}" required><br><br>
-				<input type="radio" name="gender" id="rd">&nbsp;&nbsp;&nbsp;&nbsp;<span id="but">Male</span>
-				<input type="radio" name="gender" id="rd">&nbsp;&nbsp;&nbsp;&nbsp;<span id="but">Female</span><br><br>
-				<p style="color: white;">By creating an account you agree to our <br><a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-				<input type="submit" value="Sign up" id="butto" onclick="myFunction()"><br><br>
-				<p id="sub" style="color: white;"></p>
-				<p style="color: white;">Already have an account? &nbsp;<a href="login1.php" id="log">Log In</a></p>
+				ref="login1.php" id="log">Log In</a></p>
 			</form>
 		</div>
 	</body>
