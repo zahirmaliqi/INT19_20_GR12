0e7962c1d4217b83a4635e6b1345d1e6bce7169e
vanesarama <56617704+vanesarama@users.noreply.github.com>
2020-05-08 14:57:52 +0200

    Update pic.php

--- pic.php
+++ pic.php
@@ -113,7 +113,32 @@ echo "<p style=font-size:".$this->font_size.";color:".$this->font_color.";>".$th
 $f = new MyClass2('20px','white','Women');
 
 ?></a></li>
-	<li> <a href="rinia.html" style="color:white;" >Under 23s and 18s</a> </li>
+	<li> <a href="rinia.php" style="color:white;" ><?php
+
+class Myclass3
+{
+
+private $font_size;
+private $font_color;
+private $string_value;
+
+function __construct($font_size,$font_color,$string_value)
+{
+$this->font_size = $font_size;
+$this->font_color = $font_color;
+$this->string_value = $string_value;
+$this->customize_print();
+}
+
+function customize_print()
+{
+echo "<p style=font-size:".$this->font_size.";color:".$this->font_color.";>".$this->string_value."</p>";
+} 
+}
+
+$f = new MyClass3('20px','white','Under 23s and 18s');
+
+?></a> </li>
 	
 	<li> <a href="legjendat.html" style="color:white;" >Legends</a> </li>
 	
