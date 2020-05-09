081bc91962e7482c2685ae7eaeb124017751d088
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-05-09 10:31:40 +0200

    ListaFjaleve

---
+++ lojaPHP/word.php
@@ -0,0 +1,12 @@
+<?php 
+$words = array(
+"universiteti", "arkitektura", "kompjuterika", "provimi", "karantina", 
+"programimi", "komunikimi", "seizmolog", "hipopotam", "inxhinier", 
+"temperatura", "interferenca", "parajsa", "indiferent", "propozim",
+"departament", "kontribut", "superglobalet", "koneksion", "marramendje",
+"kriptografia", "profesori", "qellimi", "abstrahim", "rafinim", 
+"cmenduri", "padrejtesi", "recesive", "kalueshmeri", 
+"enderrimtar", "perceptim", "konkluzion", "perfundimi", "spekulimi", 
+"konfuzioni", "amfiteater", "ribashkim", "barriera", "konsultime", 
+);
+?>
