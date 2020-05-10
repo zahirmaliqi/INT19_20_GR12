0f35990a21884e5e081e3ec2bd35965061a2ecc4
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-05-10 21:40:49 +0200

    Shopping cart with cookies/MySQL

---
+++ shopping.php
@@ -0,0 +1,30 @@
+<?php
+$connect = new PDO("mysql:host=localhost;dbname=product_details", "root", "");
+
+$message = '';
+
+if(isset($_POST["add_to_cart"]))
+{
+ if(isset($_COOKIE["shopping_cart"]))
+ {
+  $cookie_data = stripslashes($_COOKIE['shopping_cart']);
+
+  $cart_data = json_decode($cookie_data, true);
+ }
+ else
+ {
+  $cart_data = array();
+ }
+
+ $item_id_list = array_column($cart_data, 'item_id');
+
+ if(in_array($_POST["hidden_id"], $item_id_list))
+ {
+  foreach($cart_data as $keys => $values)
+  {
+   if($cart_data[$keys]["item_id"] == $_POST["hidden_id"])
+   {
+    $cart_data[$keys]["item_quantity"] = $cart_data[$keys]["item_quantity"] + $_POST["quantity"];
+   }
+  }
+ }
