7db6806f45c0122769e33db96ff70ddb6144c98d
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-05-10 21:45:11 +0200

    Update shopping.php

--- shopping.php
+++ shopping.php
@@ -100,3 +100,45 @@ if(isset($_GET["clearall"]))
 
 
 ?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Webslesson Demo | Simple PHP Mysql Shopping Cart</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ </head>
+ <body style=" background-image: url(only.jpeg);">
+  <br />
+  <div class="container">
+   <br />
+   <h3 align="center">Simple PHP Mysql Shopping Cart using Cookies</h3><br />
+   <br /><br />
+   <?php
+   $query = "SELECT * FROM product ORDER BY id ASC";
+   $statement = $connect->prepare($query);
+   $statement->execute();
+   $result = $statement->fetchAll();
+   foreach($result as $row)
+   {
+   ?>
+   <div class="col-md-3">
+    <form method="post">
+     <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
+      <img src="images/<?php echo $row["image"]; ?>" class="img-responsive" /><br />
+
+      <h4 class="text-info"><?php echo $row["pname"]; ?></h4>
+
+      <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>
+
+      <input type="text" name="quantity" value="1" class="form-control" />
+      <input type="hidden" name="hidden_pname" value="<?php echo $row["pname"]; ?>" />
+      <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
+      <input type="hidden" name="hidden_id" value="<?php echo $row["id"]; ?>" />
+      <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
+     </div>
+    </form>
+   </div>
+   <?php
+   }
+   ?>
