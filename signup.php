cda0a21f407124ec1a35638d6764c9e98222d21e
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-05-10 16:14:02 +0200

    Ndryshim i Signup

--- signup.php
+++ signup.php
@@ -1,4 +1,29 @@
-<!DOCTYPE html>
+<?php
+
+include("Anetari.php");
+if (! empty($_POST["regjistro"])) {
+    
+    $username = filter_var($_POST["first"], FILTER_SANITIZE_STRING);
+    $password = filter_var($_POST["pwd"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_STRING);
+    require_once ("Anetari.php");
+    $member = new Anetari();
+    $errorMessage = $member->validoAnetarin($username, $email, $password);
+    
+    if (empty($errorMessage)) {
+        $memberCount = $member->neseAnetariEkziston($username, $email);
+        
+        if ($memberCount == 0) {
+            $insertId = $member->insertoRekordet($username,$email,$password);
+            if (!empty($insertId)) {
+                header("Location: login1.php");
+            }
+        } else {
+            $errorMessage[] = "User already exists.";
+        }
+    }
+}
+?>
 <html manifest="cache.appache">
 	<head>
 		<title>Signup</title>
@@ -6,84 +31,42 @@
 		<link rel="stylesheet" href="signup.css">
 	</head>
 	<body>
-		
-			<div class="simple-form">
-             
-				<form action="inc.php" method="POST" id="registration" >
-                <h2 style="color:white;">Sign up</h2>
-                <?php
-                if(isset($_GET['first'])){
-                    $first=$_GET['first'];
-                    echo '<input type="text" name="first" placeholder="First name" id="button value="'.$first.'"><br><br>';
-                }
-                else {
-                    echo '<input type="text" name="first" placeholder="First name" id="button"><br><br>';
-                }
-                if(isset($_GET['last'])){
-                    $last=$_GET['last'];
-                    echo '<input type="text" name="last" placeholder="Last name" id="button" value="'.$last.'"><br><br>';
-                }
-                else {
-                    echo '<input type="text" name="last" placeholder="Last name" id="button"><br><br>';
-                }
-              
-                ?>
-					<input type="email" name="email" placeholder="Email" id="button"><br><br>
-					<input type="password" name="pwd" placeholder="Password" id="button"><br><br>
-				    <input type="submit" name="submit" id="butto"><br><br>
-				    <p id="sub" style="color: white;"></p>
-				    <p style="color: white;">Already have an account? &nbsp;<a href="login1.php" id="log">Log In</a></p>
-                    <?php
-             /* $fullUrl="http: //$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-              
-              if(strpos($fullUrl,"signup=empty")== true) {
-              echo "<p class='error'>You did not fill in all fields!</p>";
-              exit();
-              }
-            elseif(strpos($fullUrl,"signup=char")== true) {
-                echo "<p class='error'>You used an invalid e-mail!</p>";
-                exit();
-                }
-            elseif(strpos($fullUrl,"signup=email")== true) {
-                    echo "<p class='error'>You used invalid characters!</p>";
-                    exit();
-                    }
-            elseif(strpos($fullUrl,"signup=success")== true) {
-                        echo "<p class='success'>You have been signed up!</p>";
-                        exit();
-                        }*/
-
-                 
-                        
-                        if(!isset($_GET['signup'])){
-
-                            exit();
-                        }else {
-                            $signupCheck = $_GET['signup'];
-
-                            if($signupCheck== "empty") {
-                                echo "<p class='error'> You did not fill in all fields!</p>";
-                                exit();
-                            }
-                            elseif($signupCheck== "char") {
-                                echo "<p class='error'> You used invalid characters!</p>";
-                                exit();
-
-                            }
-                            elseif($signupCheck== "email") {
-                                echo "<p class='error'> You used an invalid email!</p>";
-                                exit();
-
-                            }                  
-                            elseif($signupCheck== "success") {
-                                echo "<p class='success'> You have been signed up!</p>";
-                                exit();
-                            }
-                        }
-                 ?>
-			</form>
-       
+	<div class="simple-form">
+<form name="frmRegistration" method="post" action="" id="registration">
+ <?php
+if (! empty($errorMessage) && is_array($errorMessage)) {
+    ?>	
+            <div style="color:red;font-weight:bold;">
+            <?php 
+            foreach($errorMessage as $message) {
+                echo $message . "<br/>";
+            }
+            ?>
+            </div>
+<?php
+}
+?>
+                    <input type="text" name="first" placeholder="First name" id="button"
+                        value="<?php if(isset($_POST['first'])) echo $_POST['first']; ?>">
+               <br><br>
             
-        	</div>
+                    <input type="email" name="email" placeholder="Email" id="button"
+                        value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>">
+                <br><br>
+           
+             <input type="password" name="pwd" placeholder="Password" id="button" value="">
+           <br><br>
+            
+                    <input type="password" name="confirm_password" placeholder="Confirm password" id="button" value="">
+               
+            <br><br>
+	 <input type="submit" name="regjistro" id="butto"><br><br>
+	<p id="sub" style="color: white;"></p>
+	 <p style="color: white;">Already have an account? &nbsp;<a href="login1.php" id="log">Log In</a></p>
+       
+     </form>
+      </div>
+        	
 	</body>
 </html>
+
