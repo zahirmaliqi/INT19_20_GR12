1121bfa05ca5f3096d21b94a81c792f48801acbe
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-05-10 21:42:10 +0200

    Update shopping.php

--- shopping.php
+++ shopping.php
@@ -28,3 +28,75 @@ if(isset($_POST["add_to_cart"]))
    }
   }
  }
+ else
+ {
+  $item_array = array(
+   'item_id'   => $_POST["hidden_id"],
+   'item_pname'   => $_POST["hidden_pname"],
+   'item_price'  => $_POST["hidden_price"],
+   'item_quantity'  => $_POST["quantity"]
+  );
+  $cart_data[] = $item_array;
+ }
+
+ 
+ $item_data = json_encode($cart_data);
+ setcookie('shopping_cart', $item_data, time() + (86400 * 30));
+ header("location:shopping.php?success=1");
+}
+
+if(isset($_GET["action"]))
+{
+ if($_GET["action"] == "delete")
+ {
+  $cookie_data = stripslashes($_COOKIE['shopping_cart']);
+  $cart_data = json_decode($cookie_data, true);
+  foreach($cart_data as $keys => $values)
+  {
+   if($cart_data[$keys]['item_id'] == $_GET["id"])
+   {
+    unset($cart_data[$keys]);
+    $item_data = json_encode($cart_data);
+    setcookie("shopping_cart", $item_data, time() + (86400 * 30));
+    header("location:shopping.php?remove=1");
+   }
+  }
+ }
+ if($_GET["action"] == "clear")
+ {
+  setcookie("shopping_cart", "", time() - 3600);
+  header("location:shopping.php?clearall=1");
+ }
+}
+
+if(isset($_GET["success"]))
+{
+ $message = '
+ <div class="alert alert-success alert-dismissible">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    Item Added into Cart
+ </div>
+ ';
+}
+
+if(isset($_GET["remove"]))
+{
+ $message = '
+ <div class="alert alert-success alert-dismissible">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  Item removed from Cart
+ </div>
+ ';
+}
+if(isset($_GET["clearall"]))
+{
+ $message = '
+ <div class="alert alert-success alert-dismissible">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  Your Shopping Cart has been clear...
+ </div>
+ ';
+}
+
+
+?>
