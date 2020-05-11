e654eb713e944086a0e4e2c6038e5fc7475400e3
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-05-11 12:56:52 +0200

    Update login1.php

--- login1.php
+++ login1.php
@@ -3,7 +3,7 @@ include_once("index.php");
 ?>
 
 <?php
-   include("dataconfi.php");
+   include("SQL/dataconfi.php");
 if(!isset($_SESSION)) 
     { 
         session_start(); 
@@ -13,24 +13,18 @@ if(!isset($_SESSION))
         session_destroy();
         session_start(); 
     }
-  // session_destroy();
+
    if(isset($_POST['login'])) {
-      // username and password sent from form 
-      
+
       $myusername = mysqli_real_escape_string($db,$_POST['email']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
       $sql = "SELECT id FROM users WHERE email ='".$myusername."' and password =md5('".$mypassword."')";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      //$active = $row['active'];
       
       $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
       if($count == 1) {
-        // session_register("myusername");
          $_SESSION['login_user'] = $myusername;
          
          header("location: Faqja1.php");
