3cfd4cde9abde9b6fde2054aed97a3982b5cb666
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-05-11 10:05:13 +0200

    Update permbajtja.php

--- signupandLogin/permbajtja.php
+++ signupandLogin/permbajtja.php
@@ -28,4 +28,73 @@ class permbajtja
         $conn->set_charset("utf8");
         return $conn;
     }
+    
+    public function select($query, $paramType="", $paramArray=array())
+    {
+        $stmt = $this->conn->prepare($query);
+        
+        if(!empty($paramType) && !empty($paramArray)) {
+            $this->bindQueryParams($sql, $paramType, $paramArray);
+        }
+        
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $resultset[] = $row;
+            }
+        }
+        
+        if (! empty($resultset)) {
+            return $resultset;
+        }
+    }
+    
+    public function insert($query, $paramType, $paramArray)
+    {
+        print $query;
+        $stmt = $this->conn->prepare($query);
+        $this->bindQueryParams($stmt, $paramType, $paramArray);
+        $stmt->execute();
+        $insertId = $stmt->insert_id;
+        return $insertId;
+    }
+   
+    public function execute($query, $paramType="", $paramArray=array())
+    {
+        $stmt = $this->conn->prepare($query);
+        
+        if(!empty($paramType) && !empty($paramArray)) {
+            $this->bindQueryParams($stmt, $paramType="", $paramArray=array());
+        }
+        $stmt->execute();
+    }
+  
+    public function bindQueryParams($stmt, $paramType, $paramArray=array())
+    {
+        $paramValueReference[] = & $paramType;
+        for ($i = 0; $i < count($paramArray); $i ++) {
+            $paramValueReference[] = & $paramArray[$i];
+        }
+        call_user_func_array(array(
+            $stmt,
+            'bind_param'
+        ), $paramValueReference);
+    }
+    
+    public function numRows($query, $paramType="", $paramArray=array())
+    {
+        $stmt = $this->conn->prepare($query);
+        
+        if(!empty($paramType) && !empty($paramArray)) {
+            $this->bindQueryParams($stmt, $paramType, $paramArray);
+        }
+        
+        $stmt->execute();
+        $stmt->store_result();
+        $recordCount = $stmt->num_rows;
+        return $recordCount;
+    }
+}
 
