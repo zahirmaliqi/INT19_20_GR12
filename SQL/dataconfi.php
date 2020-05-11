bbe68acc5e0ddfdf72a409adf4b82e2de44404ff
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-05-11 13:02:00 +0200

    Per login

---
+++ SQL/dataconfi.php
@@ -0,0 +1,8 @@
+<?php
+define("servername","localhost");
+define("dbname","signupusers");  //signupusers kur i krijojme tabelat
+define("username","root");
+define("password","");
+
+ $db = mysqli_connect(servername,username,password,dbname);
+?>
