9e13e7e8b940cf809a17c01e7c4849a4e1877f51
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-05-11 16:32:14 +0200

    Remember me with cookies

--- login1.php
+++ login1.php
@@ -1,38 +1,15 @@
 <?php
 include_once("index.php");
 ?>
-
 <?php
-   include("SQL/dataconfi.php");
-if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    } 
-	else
-    {
-        session_destroy();
-        session_start(); 
-    }
-
-   if(isset($_POST['login'])) {
-
-      $myusername = mysqli_real_escape_string($db,$_POST['email']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM users WHERE email ='".$myusername."' and password =md5('".$mypassword."')";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      
-      $count = mysqli_num_rows($result);
-      if($count == 1) {
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: Faqja1.php");
-      }
-	  else {
-         echo "Your Login Email or Password is invalid";
-      }
-   }
+if(isset($_COOKIE['email']) and isset($_COOKIE['pass'])) {
+    $email = $_COOKIE['email'];
+    $pass = $_COOKIE['pass'];
+    echo "<script>
+    document.getElementById('email').value = '$email';
+    document.getElementById('pass').value = '$pass';
+    </script>";
+}
 ?>
 <html>
     <head>
@@ -42,20 +19,17 @@ if(!isset($_SESSION))
     </head>
     <body>
     <div class="signin">
-        <form method="post" action="">
+        <form method="post" action="validate.php">
         <h2 style="color: white">Log In</h2>
-            <input type="email" name="email" id="email" placeholder="Email">
+            <input type="text" name="email" id="email" placeholder="Username">
             <input type="password" name="password" id="pass" placeholder="Password"><br><br>
                    
-                   <input type="submit" value="Login" name="login" ></a><br>
-				  <input type="checkbox" name="remember" value="1">Remember Me<br>
-     
-	 <h4>or</h4>
+                   <input type="submit"  value="Login" name="login" ></a><br>
+                   <input type="checkbox" name="remember" value="1">Remember Me<br>
+       <h4>or</h4>
 	 <?php echo $output; ?>
         </form>
-		<h5 style="padding:10px;">  Don't have an account? <a href="signup.php">Sign up!</a></h5>
-	
 </div>
-
     </body>
 </html>
+
