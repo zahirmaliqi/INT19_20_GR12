77bea049b3615ad80075b43f6861789c5336f1c0
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-05-13 16:35:51 +0200

    Fajlli per API (raste te Corona Virusit)

---
+++ corona.php
@@ -0,0 +1,20 @@
+<?php
+
+$confirmed = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Confirmed%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
+$deaths = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Deaths%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
+$recovered = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Recovered%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
+
+echo"Confirmed: ".$confirmed->features[0]->attributes->value;
+echo"<br>";
+echo"Deaths: ".$deaths->features[0]->attributes->value;
+echo"<br>";
+echo"Recovered: ".$recovered->features[0]->attributes->value;
+
+function getData($url){
+   $client = curl_init($url);
+   curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+   $response = curl_exec($client);
+   return json_decode($response);
+};
+
+?>
