46b838e1764800ae03fc0940251edfaffeddaadd
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-05-13 23:34:12 +0200

    Validimi dhe regjistrimi i te dhenave ne databaze

--- buyticket.php
+++ buyticket.php
@@ -1,35 +1,104 @@
+<?php
+	 ini_set("display_errors", 1);
+    error_reporting(E_ALL);
 
-    <?php
+
+	 $nameErr=$lastErr = $numberErr = $emailErr = $codeErr = $termsErr =$buyErr="";
+$name=$lastname = $number = $email = $code =$terms=$buy="";
+
+if(isset($_POST['add'])) {
 	
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname="myDB";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if(isset($_POST['add'])){
-$username=$_POST['username'];
-$lastname=$_POST['lastname'];
-$code=$_POST['code'];
-$email=$_POST['email'];
-$phonenumber=$_POST['number'];
-$buy=$_POST['buy'];
-if($stmt = mysqli_prepare($conn,"INSERT INTO tiketa(username,lastname,code,email,phonenumber,buy)
-VALUES (?,?,?,?,?,?)") )
+	include("dataconfi.php");
+	$error = false;
+  if (empty($_POST["username"])) {
+    $nameErr = "*Name is required";
+	$error=true;
+  } else {
+    $name =$_POST["username"];
+	if(!preg_match('/^[A-Za-z]+/',$name))
 {
-  mysqli_stmt_bind_param($stmt,"ssssss",$username,$lastname,$code,$email,$phonenumber,$buy);
+	$nameErr ="*Name is in a wrong format";
+}
+  }
   
+  if (empty($_POST["lastname"])) {
+    $lastErr = "*Lastname is required";
+	$error=true;
+  } else {
+    $lastname = $_POST["lastname"];
+	if(!preg_match('/^[A-Za-z]+/',$lastname))
+{
+	$lastErr ="*Name is in a wrong format";
+}
+  }
+  if (empty($_POST["number"])) {
+    $numberErr = "*Phone number is required";
+	$error=true;
+  }
+   else {
+    $number =$_POST["number"];
+	if(!preg_match('/[0-9]{3}-[0-9]{3}-[0-9]{4}$/',$number))
+{
+	$numberErr ="*Number is in a wrong format";
+}
+}
+ 
+if (empty($_POST["code"])) {
+    $codeErr = "*A code is required";
+	$error=true;
+  } 
+else if(strlen($_POST["code"])<6||strlen($_POST["code"])>20){
+	  $codeErr="Your code should be between 6 and 20 characters";
+	  $error=true;
+  }	
+
+  if (empty($_POST["email"])) {
+  $emailErr = "*Email is required";
+  $error=true;}
+ else {
+    $email = $_POST["email"];
+if(!preg_match('/^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/',$email))
+{
+	$emailErr ="*Email is in a wrong format";
+}
+  }
+  if (empty($_POST["buy"])) {
+    $buyErr = "*Select what to buy!";
+	$error=true;
+  } 
+  
+  if (empty($_POST["vehicle1"])) {
+    $termsErr = "*You have to accept terms and conditions to continue";
+	$error=true;
+  } 
+ 
+  else {
+	  $name=$_POST["username"];
+	  $lastname=$_POST["lastname"];
+	  $number=$_POST["number"];
+	  $email=$_POST["email"];
+	  $code=hash('md5',$_POST["code"]);
+	  $buy=$_POST["buy"];
+      $terms = $_POST["vehicle1"];
+  
+$buy=$_POST['buy'];
+if($stmt = mysqli_prepare($db,"INSERT INTO tiketa(username,lastname,code,email,phonenumber,buy) VALUES (?,?,?,?,?,?)") );
+{
+  mysqli_stmt_bind_param($stmt,"ssssss",$name,$lastname,$code,$email,$number,$buy); 
+
   if(!mysqli_stmt_execute($stmt))
   {
-	  die("nuk jan konfiguru te dhenat ".mysqli_error($conn));
+	  die("nuk jan konfiguru te dhenat ".mysqli_error($db));
   }
+echo "You are registered successfully $name $lastname <br> To get your $buy please email us at rrezeartathaqi@hotmail.com";
 
 }
-}
-mysqli_close($conn);
-    ?>
 
+}
+mysqli_close($db);
+}
+
+?>
 <html lang="en" manifest="example.appcache">
 <head>
   <meta http-equiv="content-type" content="text/html"; charset="utf-8"/>
@@ -41,11 +110,8 @@ mysqli_close($conn);
 	color:red;
 }
 </style>
-
-
     </head>
     <body>
-		<?php include("form.php"); ?>
 		
     <div class="tiketa" style="height:650px;width:250px;">
 
@@ -58,38 +124,32 @@ mysqli_close($conn);
             ?></h3>
         <h7 style="color:white">Fill the details below accurately and we will contact you soon.</h7>
             <input type="text" name="username" placeholder=" Name">
-			<span class="error"><?php echo $nameErr;?></span>
+            <span class="error"><?php echo $nameErr;?></span>
             <br><br>
             <input type="text" name="lastname" placeholder="Last Name">
             <br><br>
-            <input type="tel" name="number"
-                   placeholder="Phone number">			
-			<span class="error"><?php echo $numberErr;?></span>
-				   <br><br>
-           <input type="password" name="code"
-                   placeholder="Your code">
-			<span class="error"><?php echo $codeErr;?></span>	   
-				   <br><br>
+            <input type="tel" name="number" placeholder="Phone number">			
+	    <span class="error"><?php echo $numberErr;?></span>
+           <br><br>
+           <input type="password" name="code" placeholder="Your code">
+	   <span class="error"><?php echo $codeErr;?></span>	   
+	   <br><br>
             <input type="email" name="email" placeholder="Email address" autocomplete="off" />
-			<span class="error"><?php echo $emailErr;?></span>
-			<br><br>
-              <input type="checkbox" name="vehicle1" value="Bike"> I accept terms and condition.</input>
+	   <span class="error"><?php echo $emailErr;?></span>
+           <br><br>
+             <input type="checkbox" name="vehicle1" value="Bike"> I accept terms and condition.</input>
 			  <span class="error"><?php echo $termsErr;?></span>
 			  <br><br>
               <select name="buy" class="sty">
   <option value="Select">Select:</option>
+  <option value="T-Shirt">T-Shirt</option>
   <option value="Ticket">Ticket</option>
- 
                </select>
+	    <span class="error"><?php echo $buyErr;?></span>
                <br>  
         </br>
-            <input type="submit" formmethod="post" value="BUY NOW" onclick="myFunction()" name="add"/><br><br>
-            <h7>Please fill out all the information asked in this form!</h7>
-            <script>
-                function myFunction() {
-                alert("Are all the requirements fulfilled?");
-                                       }
-            </script>        
+            <input type="submit" formmethod="post" value="BUY NOW" name="add"/><br><br>
+            <h7>Please fill out all the information asked in this form!</h7>                  
         </form>
         </div>
     </body>
