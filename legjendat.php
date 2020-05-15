0519c274934e1cc4882a5950a78944bc649383a2
vanesarama <56617704+vanesarama@users.noreply.github.com>
2020-05-15 03:39:20 +0200

    Update legjendat.php
    include mainne1

--- legjendat.php
+++ legjendat.php
@@ -41,51 +41,8 @@ $(document).ready(function(){
   </head>
 
   <body>
-   <header style="width: 100%">
+     <?php include("menuu.php"); ?>
 
-<div class="ikonat" style="float: left;">
-		
-		<a href="https://www.linkedin.com/" class="fa fa-linkedin"></a>
-		<a href="http://twitter.com" class="fa fa-twitter"></a>
-		<a href="http://pinterest.com" class="fa fa-pinterest"></a>
-		<a href="http://google.com" class="fa fa-google"></a>
-		<a href="https://rss.com/" class="fa fa-rss"></a>
-
-  </div>
-  <div style="float: right">
-    <form>
-	     
-           <input type="text" name="text" class="search" placeholder="Search Our Website..."  >
-          
-           <input type="submit" name="submit" class="submit" value="SEARCH">
-       </form>
-   </div>
-   <br>
-   <br>
-     </header>
-
-	 <section class="faqja" >
-		<nav id="mainMenu">
-	   <div class="h6">
-		<p id="fresh"><b><big>M</big>ANCHESTER<big>U</big>NITED</b></p>
-		<p id="minfont">The best team!</p>
-      
-       </div>
-       <div >
-       <img src="mmm.png" alt="Trulli" id="man" style="width:70px; height: 70px; border-radius:50%; " >
-       </div>
-		
-		
-
-                       <ul>
-                            <li><a href="Faqja1.php" >HOMEPAGE</a></li>
-                            <li> <a href="Historyandcuriosities.php">HISTORY AND CURIOSITIES</a> </li>
-			    <li><a href="pjesa2.php">PLAYERS</a>  </li>
-                            <li>  <a href="pic.php">GALLERY</a></li>
-                            <li> <a href="Tickets.php">FIXTURES</a> </li>
-                            <li> <a href="Game.php">GAME</a> </li>
-                       </ul> 
-          </nav>
 
 
 		  <div class="ekipet">
@@ -291,81 +248,8 @@ document.getElementById("dem3").innerHTML = txt1 + " " + txt2;
 			</div>
 				
 									   
-<br/>
-<hr>
-</section>
-
-<footer>
-
-	<div >
-		<div class="row">
-
-		 
-			<div class="col">
-        <br>
-				<p >CONTACT DETAILS</p>
-        
-				
-				<p >The official Manchester United website with news,<br> fixtures, images, tickets, live match coverage,<br> match highlights, player profiles, transfers, shop <br> and more."The Red Devils"-The best sites. <br>Location: Manchester, United Kingdom<br>Arena/Stadium: Old Trafford<br>Tel: +44 161 868 8000<br>
-					Email: info@sportmanutdsoffice.com
-				</p>
-			</div>
-
-			<div class="col" >
-           <br>
-				<p  >QUICK LINKS</p>
-              
-				
-				<nav  >
-				<a  href="https://www.manutd.com/en/matches/first-team/2019-20/europa-league">Europa League</a><br>
-				<hr>
-				<a  href="https://store.manutd.com/stores/manutd/en?portal=QYTMF8KF&CMP=PSC-QYTMF8KF">UnitedDirect</a><br>
-				<hr>
-				<a  href="https://www.espn.com/soccer/team/_/id/360/manchester-united">2019-20 Prem Standings </a><br>
-				<hr>
-				<a  href="https://www.manchestereveningnews.co.uk/all-about/manchester-united-fc">Manchester Evening news</a>
-				<hr>
-				</nav>
-			</div>
-
-
-			<div  class="col">
-           <br>
-				<p >FROM THE BLOG </p>
-			
-				<p><strong>Post By Admin</strong><br>
-				Manchester United Football Club is a professional 
-				football club based in Old Trafford, Greater 
-				Manchester, England, <br>that competes in the Premier<br>
-				 League, the top flight of English football.Nicknamed "the Red Devils"<br></p>
-				<span id="readmore"><a  href="https://en.wikipedia.org/wiki/Manchester_United_F.C.">Read More&raquo;</a></span>
-			</div>
-
-			<div class="col">
-           <br>
-				<p>&nbsp;&nbsp;   GRAB OUR NEWSLETTER</p>
-				
-				<form >
-					<input type="text" name="Emri" placeholder="Name"><br>
-					<input type="text" name="Email" placeholder="Email"><br>
-					<input type="submit" name="Ruaj" value="SUBMIT" id="submit"> 
-				</form>
-
-
-			</div>
-		</div>
-		<br>
-		<hr  >
-		<div class="end">
-		<div id="cright" > Copyright &copy; 2013 - All Rights Reserved -Part Shop</div>
-    <div id="temp" >Template by OS Templates</div>
-    </div>
-		<br>
-		<br>
-
-
-    </div>
-</footer>
+<hr></section>
+<?php include("mennu1.php");?>
 	
 </body>
 
