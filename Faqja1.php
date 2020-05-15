14895d774d9ad4e547aa9456d35ebc006d896337
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-05-16 01:56:57 +0200

    Update Faqja1.php
    Check nese jemi te ldhure me internet apo jo.

--- Faqja1.php
+++ Faqja1.php
@@ -17,6 +17,18 @@
 
   <body>
    <?php include("menuu.php"); ?>
+   <?php
+$host_name = 'www.google.com';
+$port_no = '80';
+
+$st = (bool)@fsockopen($host_name, $port_no, $err_no, $err_str, 10);
+if ($st) {
+    echo "<img  src='greenlight.gif' style='width:35px;height:35px;' />";
+} else {
+    echo "<img  src='no_int.gif' style='width:100px;height:100px;' />";
+}
+?>
+	 
 		    <div class="mySlides fade">
   <img src="azk.jpeg" style="width:100%">
   <div class="text">Paul Pogba has been conspicuous in his absence from Manchester
