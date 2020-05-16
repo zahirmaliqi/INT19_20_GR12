edd96923b2f45bf3bf6f72f1fcb82f1b8b11a953
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-05-16 11:52:45 +0200

    Update rinia.php

--- rinia.php
+++ rinia.php
@@ -589,12 +589,15 @@ $f = new MyClass4('20px','white','Legends');
 			  
 		  </div>
 			  <div class="flip-box-back">
-				<p>Age: <big>19</big> </p>
-				<p>COUNTRY ENGLNAD </p>
+				<?php
 				
-				<p>DATE OF BIRTH
-					31 OCT 2000</p>
-				<p>JOINED 1 Jul 2017</p></div>
+				function rinia70($appearances, $year,$age,$sheets,$join) {
+					echo " Appearances:$appearances<br> Born in $year<br>Age: $age<br>Clean Sheets:$sheets <br>Joined:$join<br>";
+				}
+
+				rinia70("380","1990","29","131","2 Jul 2011");
+
+				?></div>
 				
 			</div>
 			  
