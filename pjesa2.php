5352e7fbf2252ebeb82eb0d9c6662ba08d429720
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-05-16 20:44:33 +0200

    Update pjesa2.php

--- pjesa2.php
+++ pjesa2.php
@@ -166,6 +166,13 @@ if ($st) {
 ?>
 	
 <span style="color: white;"  id="time"> 00:00:00 AM </span>
+<script>
+   $(document).ready(function(){
+   setInterval(function(){
+   $('#time').load('time.php')
+   },1000)
+   });
+</script>
 
 	<p class="tittleani" >
 	<!--substr() - përdor parametrat e fillimit dhe gjatësisë e krejt stringut deri ku ekzekutohet -->	
