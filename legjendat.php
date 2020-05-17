f21599cf4739e18608c203779f97220d9b727140
vanesarama <56617704+vanesarama@users.noreply.github.com>
2020-05-17 02:35:39 +0200

    Update legjendat.php

--- legjendat.php
+++ legjendat.php
@@ -43,8 +43,14 @@ $(document).ready(function(){
   <body>
      <?php include("menuu.php"); ?>
 
+      <?php
+$host_name = 'www.google.com';
+$port_no = '80';
 
-
+$st = (bool)@fsockopen($host_name, $port_no, $err_no, $err_str, 10);
+if ($st) {
+    echo "<img  src='greenlight.gif' style='width:35px;height:35px;' />";
+} 
 		  <div class="ekipet">
 <ol style="color:red">
 <li><a href="pic.php" >
