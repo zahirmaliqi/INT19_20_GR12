92a03135b56d18f3ac0f4f7fc36074d02d5c156c
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-05-17 03:44:58 +0200

    Update Tickets.php

--- Tickets.php
+++ Tickets.php
@@ -51,6 +51,15 @@ if ($st) {
     echo "<img  src='no_int.gif' style='width:100px;height:100px;' />";
 }
 ?>
+<span style="color: white;"  id="time"> 00:00:00 AM </span>
+  <script>
+    $(document).ready(function(){
+      setInterval(function(){
+        $('#time').load('time.php')
+
+      },1000)
+      });
+  </script>
 	
     <body>     
         <script>
