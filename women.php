08e9efec86e92b7bc952a1d6ee232af1a298f010
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-05-17 17:57:50 +0200

    Update women.php

--- women.php
+++ women.php
@@ -3,13 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
-   <link rel="stylesheet" href="style1.css">
-	 <link rel="stylesheet" href="titulli.css">
-	<link rel="stylesheet" type="text/css" href="zahirmaliqi.css">
-	<link rel="stylesheet" href="stylmac.css">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="titulli.css">
+    <link rel="stylesheet" type="text/css" href="zahirmaliqi.css">
+    <link rel="stylesheet" href="stylmac.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
     
     <title>Women Gallery</title>
     
